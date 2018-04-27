eff626a0571c689cb8845edba8b86f7e1a6ae901
mistasse <istassem@gmail.com>
2018-04-27 14:34:19 +0200

    Make environment picklable, add DSL for call arguments

--- mlworkflow/environment.py
+++ mlworkflow/environment.py
@@ -1,5 +1,5 @@
 from abc import ABCMeta, abstractmethod
-
+from contextlib import contextmanager
 
 _no_value = object()
 
@@ -8,6 +8,45 @@ class Evaluable(metaclass=ABCMeta):
     @abstractmethod
     def eval(self, env):
         pass
+
+
+def _transform_call_arg(value):
+    if not no_dsl.dsl_enabled:
+        return value
+
+    if isinstance(value, str):
+        if value.startswith("@"):
+            return Ref(value[1:])
+
+    return value
+
+
+@contextmanager
+def no_dsl():
+    """Disables the DSL transformation when passing call arguments
+
+    Without the DSL:
+    >>> with no_dsl():
+    ...     print(Environment(a=Call(print).with_args("compute a", "@b"),
+    ...                       b=Call(print).with_args("compute b")))
+    Environment(
+        a=Call('builtins', 'print').with_args( 'compute a', '@b' ),
+        b=Call('builtins', 'print').with_args( 'compute b' )
+    )
+
+    With the DSL again:
+    >>> print(Environment(a=Call(print).with_args("compute a", "@b"),
+    ...                   b=Call(print).with_args("compute b")))
+    Environment(
+        a=Call('builtins', 'print').with_args( 'compute a', Ref('b') ),
+        b=Call('builtins', 'print').with_args( 'compute b' )
+    )
+    """
+    _dsl = no_dsl.dsl_enabled
+    no_dsl.dsl_enabled = False
+    yield
+    no_dsl.dsl_enabled = _dsl
+no_dsl.dsl_enabled = True
 
 
 class _Call_v0(Evaluable):
@@ -42,17 +81,26 @@ class _Call_v0(Evaluable):
     def __setstate__(self, state):
         self.reference, self.args, self.kwargs = state
 
+    def __getitem__(self, key):
+        if isinstance(key, tuple):
+            key0 = key[0]
+            key1 = key[1] if len(key) == 2 else key[1:]
+            return self.kwargs[key0][key1]
+        return self.kwargs[key]
+
     def on(self, callable, modattr=None):
-        return Call(callable, modattr, args=self.args, kwargs=self.kwargs)
+        return _Call_v0(callable, modattr, args=self.args, kwargs=self.kwargs)
 
     def with_args(self, *args):
-        new_args = sum(((arg,) if arg is not Ellipsis else self.args
+        new_args = sum(((_transform_call_arg(arg),)
+                        if arg is not Ellipsis else self.args
                         for arg in args), ())
-        return Call(*self.reference, args=new_args, kwargs=self.kwargs)
+        return _Call_v0(*self.reference, args=new_args, kwargs=self.kwargs)
 
     def __call__(self, **kwargs):
-        new_kwargs = {**self.kwargs, **kwargs}
-        return Call(*self.reference, args=self.args, kwargs=new_kwargs)
+        new_kwargs = {**self.kwargs, **{k: _transform_call_arg(v)
+                                        for k, v in kwargs.items()}}
+        return _Call_v0(*self.reference, args=self.args, kwargs=new_kwargs)
 
     @property
     def resolved_target(self):
@@ -97,7 +145,7 @@ class _Call_v0(Evaluable):
             s.append("(")
             s.append(nl_indent)
             for k, v in self.kwargs.items():
-                _v = str(v) if isinstance(v, Call) else repr(v)
+                _v = str(v) if isinstance(v, _Call_v0) else repr(v)
                 head = k+"="
                 s.append(head)
                 _nl_indent = nl_indent + " "*len(head)
@@ -109,7 +157,7 @@ class _Call_v0(Evaluable):
             s.append(".with_args(")
             s.append(nl_indent)
             for arg in self.args:
-                _v = str(arg) if isinstance(arg, Call) else repr(arg)
+                _v = str(arg) if isinstance(arg, _Call_v0) else repr(arg)
                 s.append(nl_indent.join(_v.split("\n")))
                 s.append(",\n" + indentation)
             s[-1] = s[-1][1:-1]  # remove comma and space
@@ -143,7 +191,7 @@ class _Unique_v0(_Ref_v0):
 class _Environment_v0(dict):
     """A malleable and persistent representation for a computation graph
 
-    >>> env = Environment(a=Call(print).with_args("compute a", Ref("b")),
+    >>> env = Environment(a=Call(print).with_args("compute a", "@b"),
     ...                   b=Call(print).with_args("compute b"))
     >>> env.run("a")
     compute b
@@ -158,15 +206,28 @@ class _Environment_v0(dict):
     >>> env.run(["a", "b"])
     compute a None
     [None, None]
+
+    >>> import pickle
+    >>> s = pickle.dumps(env)
+    >>> pickle.loads(s).run(["a", "b"])
+    compute b
+    compute a None
+    [None, None]
     """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.cache = {}
 
+    def __getstate__(self):
+        return dict(self)
+
+    def __setstate__(self, state):
+        return self.__init__(state)
+
     @property
     def fused(self):
-        return Environment({**self, **self.cache})
+        return _Environment_v0({**self, **self.cache})
 
     def run(self, name):
         if isinstance(name, list):
@@ -190,7 +251,7 @@ class _Environment_v0(dict):
                 head = "{}=".format(k)
                 s.append(head)
                 _nl_indent = nl_indent + " "*len(head)
-                _v = str(v) if isinstance(v, (Call, Environment)) else repr(v)
+                _v = str(v) if isinstance(v, (_Call_v0, _Environment_v0)) else repr(v)
                 s.append(_nl_indent.join(_v.split("\n")))
                 s.append(",")
             s[-1] = ""
