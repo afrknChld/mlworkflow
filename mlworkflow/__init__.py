293c1d207f0a3e1860cd380eba817c4a0c3d32bf
mistasse <istassem@gmail.com>
2018-04-30 12:12:31 +0200

    Added _env reference and code execution evaluable

--- mlworkflow/__init__.py
+++ mlworkflow/__init__.py
@@ -4,7 +4,7 @@ from mlworkflow.datasets import Dataset, AugmentedDataset, TransformedDataset,\
     PickledDataset, pickle_or_load
 
 from mlworkflow.data_collection import DataCollection, find_files
-from mlworkflow.environment import Call, Ref, Environment
+from mlworkflow.environment import Call, Exec, Ref, Environment
 from mlworkflow.interactive import LivePanels
 from mlworkflow.notebook import Notebook
 from mlworkflow.keras import get_keras_weights, set_keras_weights
