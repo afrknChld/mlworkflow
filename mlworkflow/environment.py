1daef1d16c992b5eb2dbbd59e51d11011e43510f
mistasse <istassem@gmail.com>
2018-05-02 11:19:48 +0200

    Added Evaluable global refs

--- mlworkflow/environment.py
+++ mlworkflow/environment.py
@@ -1,5 +1,6 @@
 from abc import ABCMeta, abstractmethod
 from contextlib import contextmanager
+import re
 
 _no_value = object()
 
@@ -19,6 +20,23 @@ def _transform_call_arg(value):
             return Ref(value[1:])
 
     return value
+
+
+def _resolve_attr(obj, path, *, first_accessor=None):
+    split = re.split(r"([\.@>])", path)
+    accessors = split[1::2]
+    keys = split[::2]
+    if first_accessor is not None:
+        accessors.insert(0, first_accessor)
+    assert len(accessors) == len(keys)
+    for acc, key in zip(accessors, keys):
+        if acc == ".":
+            obj = getattr(obj, key)
+        elif acc == "@":
+            obj = obj[key]
+        elif acc == ">":
+            obj = obj.run(key)
+    return obj
 
 
 @contextmanager
@@ -49,6 +67,52 @@ def no_dsl():
 no_dsl.dsl_enabled = True
 
 
+class GlobalRef(Evaluable):
+    def __init__(self, module, path):
+        self.module = module
+        self.path = path
+
+    def __reduce__(self):
+        return GlobalRef._v0, (self.module, self.path,)
+    
+    def eval(self, env=None):
+        import importlib
+        module = importlib.import_module(self.module)
+        return _resolve_attr(module, self.path, first_accessor=".")
+
+    def __repr__(self):
+        return "GlobalRef({!r}, {!r})".format(self.module, self.path)
+
+    @staticmethod
+    def _v0(module, path):
+        return GlobalRef(module, path)
+
+
+class Ref(Evaluable):
+    """A reference to a root element of a computation graph"""
+
+    def __init__(self, path):
+        self.path = path
+
+    def __reduce__(self):
+        return Ref._v0, (self.path,)
+
+    def eval(self, env):
+        return _resolve_attr(env, self.path, first_accessor=">")
+
+    def __eq__(self, other):
+        if not isinstance(other, Ref):
+            return False
+        return self.path == other.path
+
+    def __repr__(self):
+        return "Ref({!r})".format(self.path)
+
+    @staticmethod
+    def _v0(path):
+        return Ref(path)
+
+
 class Call(Evaluable):
     """A malleable and picklable representation for a call.
 
@@ -69,9 +133,13 @@ class Call(Evaluable):
 
     def __init__(self, callable, modattr=None, *, args=(), kwargs={}):
         if modattr is None:
-            self.reference = (callable.__module__, callable.__name__)
+            if isinstance(callable, Evaluable):
+                self.reference = callable
+            else:
+                self.reference = GlobalRef(callable.__module__,
+                                           callable.__name__)
         else:
-            self.reference = (callable, modattr)
+            self.reference = GlobalRef(callable, modattr)
         self.args = args
         self.kwargs = kwargs
 
@@ -92,32 +160,20 @@ class Call(Evaluable):
         new_args = sum(((_transform_call_arg(arg),)
                         if arg is not Ellipsis else self.args
                         for arg in args), ())
-        return Call(*self.reference, args=new_args, kwargs=self.kwargs)
+        return Call(self.reference, args=new_args, kwargs=self.kwargs)
 
     def __call__(self, **kwargs):
         new_kwargs = {**self.kwargs, **{k: _transform_call_arg(v)
                                         for k, v in kwargs.items()}}
-        return Call(*self.reference, args=self.args, kwargs=new_kwargs)
-
-    @property
-    def resolved_target(self):
-        import importlib
-        call_target = getattr(self, "_call_target", _no_value)
-        if call_target is not _no_value:
-            return call_target
-        module, attrs = self.reference
-        current = importlib.import_module(module)
-        for attr in attrs.split("."):
-            current = getattr(current, attr)
-        call_target = self._call_target = current
-        return call_target
+        return Call(self.reference, args=self.args, kwargs=new_kwargs)
 
     def eval(self, env=None):
+        ref = self.reference.eval(env)
         args = [arg.eval(env) if isinstance(arg, Evaluable) else arg
                 for arg in self.args]
         kwargs = {k: v.eval(env) if isinstance(v, Evaluable) else v
                   for k, v in self.kwargs.items()}
-        return self.resolved_target(*args, **kwargs)
+        return ref(*args, **kwargs)
 
     def __eq__(self, other):
         if not isinstance(other, Call):
@@ -125,6 +181,13 @@ class Call(Evaluable):
         return (self.reference == other.reference and
                 self.args == other.args and
                 self.kwargs == other.kwargs)
+
+    def _format_ref(self):
+        if isinstance(self.reference, GlobalRef):
+            return "{!r}, {!r}".format(self.reference.module,
+                                       self.reference.path)
+        else:
+            return repr(self.reference)
 
     def __repr__(self):
         kwargs = args = ""
@@ -134,10 +197,10 @@ class Call(Evaluable):
         if self.kwargs:
             kwargs = "({})".format(", ".join("{}={!r}".format(k, v)
                                              for k, v in self.kwargs.items()))
-        return "Call{!r}{}{}".format(self.reference, args, kwargs)
+        return "Call({}){}{}".format(self._format_ref(), args, kwargs)
 
     def __str__(self):
-        s = ["Call{!r}".format(self.reference)]
+        s = ["Call({})".format(self._format_ref())]
         indentation = " "*2
         nl_indent = "\n{}".format(indentation)
         if self.kwargs:
@@ -164,38 +227,8 @@ class Call(Evaluable):
         return "".join(s)
 
     @staticmethod
-    def _v0(reference, args, kwargs):
-        return Call(*reference, args=args, kwargs=kwargs)
-
-
-class Ref(Evaluable):
-    """A reference to a root element of a computation graph"""
-
-    def __init__(self, name, *keys):
-        self.name = name
-        self.keys = keys
-
-    def __reduce__(self):
-        return Ref._v0, (self.name,) + self.keys
-
-    def eval(self, env):
-        item = env.run(self.name)
-        for k in self.keys:
-            item = item[k]
-        return item
-
-    def __eq__(self, other):
-        if not isinstance(other, Ref):
-            return False
-        return self.name == other.name and self.keys == other.keys
-
-    def __repr__(self):
-        return "Ref({})".format(", ".join(repr(k)
-                                          for k in ((self.name,) + self.keys)))
-
-    @staticmethod
-    def _v0(name):
-        return Ref(name)
+    def _v0(evaluable, args, kwargs):
+        return Call(evaluable, args=args, kwargs=kwargs)
 
 
 class Exec(Evaluable, dict):
@@ -226,18 +259,18 @@ class Exec(Evaluable, dict):
     ... #@export d
     ... d = someValues["a"] + someValues["b"]
     ... e = d + someValues["a"] + someValues["b"]
-    ... ''', cross_refs=True)
+    ... ''', gen_refs=True)
     >>> env.run("y") == {"d": 3}
     True
     >>> env.run("d")
     3
     """
-    def __init__(self, code, cross_refs=False):
+    def __init__(self, code, gen_refs=False):
         self.code = code
-        self.cross_refs = cross_refs
+        self.gen_refs = gen_refs
 
     def __reduce__(self):
-        return Exec._v0, (self.code, self.cross_refs)
+        return Exec._v0, (self.code, self.gen_refs)
 
     def eval(self, env=None):
         _result = _no_value
@@ -257,11 +290,11 @@ class Exec(Evaluable, dict):
             exported = set(k for k in locs if not k.startswith("_"))
         # Only retain exported variables
         locs = {k:v for k, v in locs.items() if k in exported}
-        if self.cross_refs:
+        if self.gen_refs:
             assert env[env.current] == self, ("Cannot run a non root Exec "
-                                              "with cross_refs option")
+                                              "with gen_refs option")
             for n in locs:
-                ref = Ref(env.current, n)
+                ref = Ref("{}@{}".format(env.current, n))
                 current_item = env.get(n, _no_value)
                 assert current_item is _no_value or ref == current_item, \
                     ("Variable {!r} was already defined in field {!r}." 
@@ -284,15 +317,15 @@ class Exec(Evaluable, dict):
                 raise KeyError(key)
 
     def __str__(self):
-        return ("Exec('''\n{}\n''', cross_refs={!r})"
-                .format(self.code.replace("'''", r'\'\'\''), self.cross_refs))
+        return ("Exec('''\n{}\n''', gen_refs={!r})"
+                .format(self.code.replace("'''", r'\'\'\''), self.gen_refs))
 
     def __repr__(self):
-        return "Exec({!r}, cross_refs={!r})".format(self.code, self.cross_refs)
+        return "Exec({!r}, gen_refs={!r})".format(self.code, self.gen_refs)
 
     @staticmethod
-    def _v0(code, cross_refs=False):
-        return Exec(code, cross_refs=cross_refs)
+    def _v0(code, gen_refs=False):
+        return Exec(code, gen_refs=gen_refs)
             
 
 class Environment(dict):
@@ -418,7 +451,7 @@ else:
         def with_env(line, cell):
             env, name, *flags = line.split(" ")
             env = _ip.user_global_ns[env]
-            env[name] = Exec(cell, cross_refs=True)
+            env[name] = Exec(cell, gen_refs=True)
             res = env.run(name)
             if "silent" not in flags:
                 return res
