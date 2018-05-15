9bd3898082d8f459b353fa1a95e0e91c5d0b7b95
mistasse <istassem@gmail.com>
2018-05-15 15:03:53 +0200

    Version 0.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="Istasse M.",
     author_email="istassem@gmail.com",
     license='MIT',
-    version="0.0.2",
+    version="0.0.3",
     python_requires='>=3.6',
     description="A workflow-improving library for manipulating ML experiments",
     long_description_content_type="text/markdown",
