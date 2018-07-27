3f7e7d1e1263749862f472ddc402b1569bddf05d
mistasse <istassem@gmail.com>
2018-07-27 16:25:08 +0200

    Version 0.0.5

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ from os import path
 
 setup(
     name="mlworkflow",
-    author="Istasse M.",
+    author="Maxime Istasse",
     author_email="istassem@gmail.com",
+    url="https://github.com/mistasse/mlworkflow",
     license='MIT',
-    version="0.0.3",
+    version="0.0.5",
     python_requires='>=3.6',
     description="A workflow-improving library for manipulating ML experiments",
     long_description_content_type="text/markdown",
     packages=find_packages(include=("mlworkflow",)),
-    install_requires=["numpy", "tqdm", "ipywidgets"]
+    install_requires=["numpy", "tqdm", "ipywidgets"],
 )
